e467904911cfeea5e5827d0ebca1fed7d0cf8fdf
ElianaElshani <st184472@stud.uni-stuttgart.de>
2026-04-01 13:59:05 +0200

    Low Performer Fixations

--- scripts/testA/fixations_plot.py
+++ scripts/testA/fixations_plot.py
@@ -13,12 +13,12 @@ matplotlib.use("Agg")
 # ----------------------------------------------------
 # Configuration
 # ----------------------------------------------------
-PARTICIPANT = "Participant21"
-QUESTION_ID = 1
+PARTICIPANT = "Participant 10"
+QUESTION_ID = 7
 
 DATA_FILE = os.path.join("..", "..", "data", "testA", f"{PARTICIPANT}.tsv")
 IMAGE_PATH = os.path.join("..", "..", "data", "testA", "stimuli", f"Question{QUESTION_ID}.png")
-OUTPUT_DIR = os.path.join("..", "..", "results", "testA", PARTICIPANT.lower(), "fixations")
+OUTPUT_DIR = os.path.join("..", "..", "results", "testA", PARTICIPANT.lower(), "fixations_clean")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
